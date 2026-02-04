3e6a7bef8552665589980ec8d9168246e44ca7ca
Mahmoud Abdallah Hassan <Mahmoud-Abdallah@shakeregypt.com>
2026-02-04 16:42:18 +0200

    Last update

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,22 +1,29 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     // Field
     private WebDriver driver;
+    private By linksLocator = By.tagName("a");
 
     // Method
     public void setup(){
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
-//        driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(430,932));
         System.out.println(driver.getTitle());
-//        driver.close();
-        driver.quit();
+        List<WebElement> links = driver.findElements(linksLocator);
+        System.out.println(links.size());
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+//        driver.quit();
     }
 
     // Main
