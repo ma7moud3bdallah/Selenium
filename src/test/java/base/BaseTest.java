630bbf0e27ff67bc2fbcf12f2161f72a3882c3a5
Mahmoud Abdallah Hassan <Mahmoud-Abdallah@shakeregypt.com>
2026-02-24 10:59:56 +0200

    Last update

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,11 +1,19 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseTest {
@@ -25,8 +33,20 @@ public class BaseTest {
     public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
     }
+    @AfterMethod
+    public void takeScreenshot(ITestResult result){
+        if(ITestResult.FAILURE==result.getStatus()){
+            TakesScreenshot camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot,new File("resources/screenshots/"+result.getName()+".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @AfterClass
     public void tearDown(){
-//        driver.quit();
+        driver.quit();
     }
 }
